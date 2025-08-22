d9afd5784c118826a3cb9f8541cc258b5d5b93af
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-08-22 07:15:30 +0530

    Add initial project structure with .gitignore, main application, and test classes

---
+++ Spring Boot Course API/src/main/java/com/krishnaallu009/springbootQiuckStart/App.java
@@ -0,0 +1,13 @@
+package com.krishnaallu009.springbootQiuckStart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
